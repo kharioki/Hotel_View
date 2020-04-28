3736924f77a7768314089c9d220aa16915396cd6
kharioki <antolisah@gmail.com>
2020-04-29 01:30:22 +0300

    added stats view

--- screens/HotelView/index.js
+++ screens/HotelView/index.js
@@ -5,6 +5,7 @@ import {colors} from '../../styles';
 import Header from './components/Header';
 import Bookmark from './components/Bookmark';
 import About from './components/About';
+import Stats from './components/Stats';
 
 export default function HotelView() {
   return (
@@ -16,6 +17,7 @@ export default function HotelView() {
       <View>
         <Bookmark />
         <About />
+        <Stats />
       </View>
     </View>
   );
