83a98fb8c776aecbe30a805213224cc81a86b6e0
kharioki <antolisah@gmail.com>
2020-04-29 03:51:07 +0300

    hotel address view

--- screens/HotelView/index.js
+++ screens/HotelView/index.js
@@ -7,6 +7,7 @@ import Bookmark from './components/Bookmark';
 import About from './components/About';
 import Stats from './components/Stats';
 import Amenities from './components/Amenities';
+import Address from './components/Address';
 
 export default function HotelView() {
   return (
@@ -20,6 +21,7 @@ export default function HotelView() {
         <About />
         <Stats />
         <Amenities />
+        <Address />
       </View>
     </View>
   );
--- styles.js
+++ styles.js
@@ -43,4 +43,22 @@ export const gs = StyleSheet.create({
     borderBottomWidth: 1,
     marginVertical: 24,
   },
+  smallText: {
+    fontSize: 12,
+    fontWeight: '800',
+    color: colors.text,
+  },
+  absoluteFull: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.pink,
+    borderRadius: 100,
+  },
 });
